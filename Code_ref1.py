f4959e19fddf3e737c971f5c1886b499af954819
BALU <61798831+Balagopala250@users.noreply.github.com>
2022-07-28 02:10:37 +0530

    Update Code_ref1.py

--- Code_ref1.py
+++ Code_ref1.py
@@ -1,1 +1,22 @@
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder \
+         .master('yarn') \
+         .appName("testing") \
+         .getOrCreate()
+
+
+#spark-submit "file_path"
+#--master
+#Learn about cluster Manager --> yarn mesos kubernetes local local[k]
+#deploy-mode 
+
+#Cluster  and client 
+
+# 
+#
+#
+#
+#
 
